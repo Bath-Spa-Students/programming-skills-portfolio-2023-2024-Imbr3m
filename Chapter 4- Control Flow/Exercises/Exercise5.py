c67ec0babc04b0773f2027de42577e379050555a
rainer <rainertoviel@gmail.com>
2024-03-16 18:51:47 +0400

    the rest of chapter 4 exercises

---
+++ Chapter 4- Control Flow/Exercises/Exercise5.py
@@ -0,0 +1,16 @@
+favorite_fruits = ["Op-Op Fruit", "Rumble-Rumble Fruit", "Dark-Dark Fruit"]
+
+if "Op-Op Fruit" in favorite_fruits:
+    print("You are an Op-Op Fruit user!")
+
+if "banana" in favorite_fruits:
+    print("You really like bananas!")
+
+if "Dark-Dark Fruit" in favorite_fruits:
+    print("You control Darkness, a Dark-Dark Fruit user!")
+
+if "Rumble-Rumble Fruit" in favorite_fruits:
+    print("Thunder! Lightning! Rumble-Rumble Fruit!")
+
+if "grape" in favorite_fruits:
+    print("You really like grapes!")
