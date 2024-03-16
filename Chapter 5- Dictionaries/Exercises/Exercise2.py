8b047e3d70b311fbeabf4f8785c4e48852d5dbbb
rainer <rainertoviel@gmail.com>
2024-03-16 19:16:03 +0400

    somthing wrong

---
+++ Chapter 5- Dictionaries/Exercises/Exercise2.py
@@ -0,0 +1,28 @@
+# a glossary dictionary
+glossary = {
+    'loop': 'A control flow statement that allows code to be executed repeatedly based on a condition.',
+    'function': 'A block of organized, reusable code that performs a specific task.',
+    'variable': 'A named storage location in computer memory where data can be stored and accessed.',
+    'list': 'A data structure that holds an ordered collection of items, which can be of different types.',
+    'if statement': 'A statement that performs different actions depending on whether a condition is true or false.'
+}
+
+# assigning the word and using it as a key to get the meaning from the glossary
+word = 'variable'
+print(f"{word.title()}: {glossary[word]}\n")
+
+# assigning the word and using it as a key to get the meaning from the glossary
+word = 'function'
+print(f"{word.title()}: {glossary[word]}\n")
+
+# assigning the word and using it as a key to get the meaning from the glossary
+word = 'variable'
+print(f"{word.title()}: {glossary[word]}\n")
+
+# assigning the word and using it as a key to get the meaning from the glossary
+word = 'list'
+print(f"{word.title()}: {glossary[word]}\n")
+
+# assigning the word and using it as a key to get the meaning from the glossary
+word = 'if statement'
+print(f"{word.title()}: {glossary[word]}\n")
